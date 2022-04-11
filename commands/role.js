f3bd87c1e942f28841d97f2363e71b52c5eb40c2
Ethan Partidas <ethanpartidas@gmail.com>
2022-04-11 15:40:25 -0400

    fix indentation

--- commands/role.js
+++ commands/role.js
@@ -5,23 +5,23 @@ module.exports = {
 	description: 'adds and removes roles',
 
 	execute(message, args) {
-    const arg = args.length > 0 ? args[0].toLowerCase() : '';
+		const arg = args.length > 0 ? args[0].toLowerCase() : '';
 
-    if (arg === 'list') {
-      let reply = 'Roles:\n';
-      for (const role in roles) {
-        reply += '- ' + role[0].toUpperCase() + role.slice(1) + '\n';
-      }
-      message.channel.send(reply);
-      return;
-    }
+		if (arg === 'list') {
+			let reply = 'Roles:\n';
+			for (const role in roles) {
+				reply += '- ' + role[0].toUpperCase() + role.slice(1) + '\n';
+			}
+			message.channel.send(reply);
+			return;
+		}
 
-    const roleId = roles[arg];
+		const roleId = roles[arg];
 		if (roleId === undefined)
-      message.channel.send('That role is invalid. Try using "!role list" to see a list of valid roles.');
-    else if (message.member.roles.cache.has(roleId))
-      message.member.roles.remove(roleId);
-    else
-      message.member.roles.add(roleId);
+			message.channel.send('That role is invalid. Try using "!role list" to see a list of valid roles.');
+		else if (message.member.roles.cache.has(roleId))
+			message.member.roles.remove(roleId);
+		else
+			message.member.roles.add(roleId);
 	}
 }
