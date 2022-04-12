6bb6a1d46291ed43103821dedc4c11cab0244d6d
Ethan Partidas <ethanpartidas@gmail.com>
2022-04-11 20:12:19 -0400

    fix indentation

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -7,38 +7,38 @@ module.exports = {
 	description: 'adds roles with reactions',
 	
 	async execute(message, args, client) {
-    client.on('messageReactionAdd', async (reaction, user) => {
-      if (reaction.message.partial) await reaction.message.fetch();
-      if (reaction.partial) await reaction.fetch();
-      if (user.bot) return;
-      if (!reaction.message.guild) return;
-      if (reaction.message.channel.id !== channels.roles) return;
-      for (const role in roles)
-        if (reaction.emoji.name === roles[role].emoji)
-          await reaction.message.guild.members.cache.get(user.id).roles.add(roles[role].id);
-    });
-    client.on('messageReactionRemove', async (reaction, user) => {
-      if (reaction.message.partial) await reaction.message.fetch();
-      if (reaction.partial) await reaction.fetch();
-      if (user.bot) return;
-      if (!reaction.message.guild) return;
-      if (reaction.message.channel.id !== channels.roles) return;
-      for (const role in roles)
-        if (reaction.emoji.name === roles[role].emoji)
-          await reaction.message.guild.members.cache.get(user.id).roles.remove(roles[role].id);
-    });
+		client.on('messageReactionAdd', async (reaction, user) => {
+			if (reaction.message.partial) await reaction.message.fetch();
+			if (reaction.partial) await reaction.fetch();
+			if (user.bot) return;
+			if (!reaction.message.guild) return;
+			if (reaction.message.channel.id !== channels.roles) return;
+			for (const role in roles)
+				if (reaction.emoji.name === roles[role].emoji)
+					await reaction.message.guild.members.cache.get(user.id).roles.add(roles[role].id);
+		});
+		client.on('messageReactionRemove', async (reaction, user) => {
+			if (reaction.message.partial) await reaction.message.fetch();
+			if (reaction.partial) await reaction.fetch();
+			if (user.bot) return;
+			if (!reaction.message.guild) return;
+			if (reaction.message.channel.id !== channels.roles) return;
+			for (const role in roles)
+				if (reaction.emoji.name === roles[role].emoji)
+					await reaction.message.guild.members.cache.get(user.id).roles.remove(roles[role].id);
+		});
 
-    if (message.channel.id !== channels.roles) {
-      message.channel.send('Please use the roles channel.');
-      return;
-    }
+		if (message.channel.id !== channels.roles) {
+			message.channel.send('Please use the roles channel.');
+			return;
+		}
 
 		const embed = new MessageEmbed()
-      .setTitle('Reaction Roles')
-      .setDescription('React to your emoji to get your role!')
-    let botMessage = await message.channel.send({ embeds: [embed] });
+			.setTitle('Reaction Roles')
+			.setDescription('React to your emoji to get your role!')
+		let botMessage = await message.channel.send({ embeds: [embed] });
 
-    for (const role in roles)
-      botMessage.react(roles[role].emoji);
+		for (const role in roles)
+			botMessage.react(roles[role].emoji);
 	}
 }
